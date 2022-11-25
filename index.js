7c9894c895a9e4fa9d0f0d538e6259349dab280b
tony555t <tonytullah764@gmail.com>
2022-11-25 22:57:42 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(someWhere="roller-skate") {
+    return `This Saturday, I want to ${someWhere}!`
+    }
+    saturdayFun()
+     
+    let mondayWork = function(willDo="go to the office"){
+        return `This Monday, I will ${willDo}.` 
+       }
+       mondayWork()
+       function wrapAdjective (type="*") {
+        return function(stringOne="special"){
+          return `You are ${type}${stringOne}${type}!`
+        }
+      }
